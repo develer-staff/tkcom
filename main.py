d132092132399c6faf04b7cbeb97b4f37046657e
Inox <dinnocenti@develer.com>
2019-07-09 18:10:56 +0200

    aggiunto thread di lettura

--- main.py
+++ main.py
@@ -8,30 +8,27 @@ import threading
 
 
 window = Tk() 
-window.geometry('640x480')
+window.geometry('400x480')
 window.title("tkcom")
 cb_ports = Combobox(window, width=10)
 
 def fill_ports():
     for p in list(serial.tools.list_ports.comports()):
         cb_ports['values'] += str(p).split(" ")[0]
-    cb_ports.grid(column=0, row=0)
+    cb_ports.grid(column=1, row=0)
 
 def btn_ports_clicked():
     t = threading.Thread(target=read_port)
     t.start()
-
-         
+       
 def read_port():
      with serial.Serial(cb_ports.get(), 9600, timeout=1) as ser:      
             while 1:
-                #x = ser.read()          # read one byte
-                #s = ser.read(10)        # read up to ten bytes (timeout)
-                line = ser.readline()   # read a '\n' terminated line
-                print(line)
-                txt.insert(INSERT,line)
+                txt.insert(INSERT,ser.readline())
                 window.update()   
 
+lbl_port = Label(window,text="Select serial port:")
+lbl_port.grid(column=0,row=0)
 btn = Button(window, text="Click Me", command=btn_ports_clicked)
 btn.grid(column=0, row=20)
 txt = scrolledtext.ScrolledText(window,width=40,height=10)
